976c3b06a7cdc776daa792572382479021926d71
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-12 21:18:29 +0800

    updated

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,3 +18,9 @@
 
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/omni_Archimedes.mk
+
+# Android OS Branches
+COMMON_LUNCH_CHOICES := \
+     omni_Archimedes-user \
+     omni_Archimedes-userdebug \
+     omni_Archimedes-eng
